56124925335370ccf8c7f68c071f6c5caec60b4f
deepak-patil27 <mr.deepak.patil@outlook.com>
2022-08-31 00:56:44 +0530

    Refactored: Rest Call To Show Hello Mark

--- src/main/java/com/bridgelabz/restapicalls/controller/HelloRestController.java
+++ src/main/java/com/bridgelabz/restapicalls/controller/HelloRestController.java
@@ -1,6 +1,8 @@
 package com.bridgelabz.restapicalls.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,5 +11,9 @@ import org.springframework.web.bind.annotation.RestController;
     @RequestMapping(value = {"", "/", "/home"})
     public String sayHello() {
         return "Hello From Bridgelabz";
+    }
+    @RequestMapping(value = {"/query"}, method = RequestMethod.GET)
+    public String sayHello(@RequestParam(value = "name") String name) {
+        return "Hello " + name + "!";
     }
 }
